9d9a0985bc5a36c2f6b3b219bccb45ef48316568
unknown <F-shakalaka>
2019-05-24 09:28:50 +0800

    123123

---
+++ .spyder-py3/history.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Mar 25 19:17:04 2019)---
+runfile('C:/Users/25696/Desktop/lsqstock/stockFigure.py', wdir='C:/Users/25696/Desktop/lsqstock')
+
+## ---(Mon Apr  1 19:41:14 2019)---
+runfile('C:/Users/25696/Desktop/pythonspace/sz_test.py', wdir='C:/Users/25696/Desktop/pythonspace')
+runfile('C:/pythonspace/sz_test.py', wdir='C:/pythonspace')
